e704a2f290c48cd8391706474c53fa637f335dd7
Max Widhalm <max.widhalm23@d155.org>
2022-09-06 08:04:17 -0500

    Initial Commit

---
+++ Mod2AppCalculator_Widhalm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod2AppCalculator_Widhalm
+//
+//  Created by MAX WIDHALM on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
